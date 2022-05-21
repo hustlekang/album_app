dc1055cf742d919996c911d2e7c83e835cbf56f2
hustlekang <rkdgusrn9688@gmail.com>
2022-05-21 19:11:35 +0900

    Feat: 네이게이션바 구현

---
+++ components/NavBar.js
@@ -0,0 +1,44 @@
+import { useSelector,useDispatch } from "react-redux"
+import { setPage } from "../store/modules/album";
+
+export default function NavBar() {
+  const dispatch = useDispatch();
+  const albumCnt  = useSelector(({album})=>album.albums.length);
+  const pageCnt = Math.ceil(albumCnt/5);
+  const pages = [...Array(pageCnt)].map((v,i) => i); // Array(pageCnt).map하면 렌더링이 안됨;
+
+  const changePage = (e) => {
+    dispatch(setPage(e.target.id));
+  }
+
+  return (
+    <div>
+    {pages.map((page,index) => <button
+      key={index} 
+      id={page+1} 
+      onClick={changePage}>
+        {page+1}
+      </button>
+    )}
+
+    <style jsx>{`
+      div{
+        display: flex;
+        padding: 5px;
+      }
+      button{
+        width: 100%;
+        height: 30px;
+        background-color: #bdcfdf;
+        border: 1px solid rgb(62, 66, 129);
+        border-radius: 1px;
+        cursor: pointer;
+        border-radius: 3px;
+      }
+      button:hover{
+        background-color: white;
+      }
+    `}</style>
+  </div>
+  )
+}
